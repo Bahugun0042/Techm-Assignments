53576dedf4746dc81180810ae3d66f771e7d6d41
Bahugun Sai Chapalamadugu <126312249+Bahugun0042@users.noreply.github.com>
2025-02-28 01:38:36 +0530

    Add files via upload

---
+++ Daily_Tasks/2025-02-26/typescript/Task1.js
@@ -0,0 +1,4 @@
+var PName = "John Doe";
+var PAge = 30;
+console.log("Name: ".concat(PName));
+console.log("Age: ".concat(PAge));
---
+++ Daily_Tasks/2025-02-26/typescript/Task1.ts
@@ -0,0 +1,5 @@
+let PName: string = "John Doe";
+let PAge: number = 30;
+
+console.log(`Name: ${PName}`);
+console.log(`Age: ${PAge}`);

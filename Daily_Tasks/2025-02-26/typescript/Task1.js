17a9cb90d91a1eb2eef6ca719fb7351137294c9e
Bahugun Sai Chapalamadugu <126312249+Bahugun0042@users.noreply.github.com>
2025-03-02 21:21:42 +0530

    Delete Daily_Tasks/2025-02-26 directory

--- Daily_Tasks/2025-02-26/typescript/Task1.js
+++
@@ -1,4 +0,0 @@
-var PName = "John Doe";
-var PAge = 30;
-console.log("Name: ".concat(PName));
-console.log("Age: ".concat(PAge));
--- Daily_Tasks/2025-02-26/typescript/Task1.ts
+++
@@ -1,5 +0,0 @@
-let PName: string = "John Doe";
-let PAge: number = 30;
-
-console.log(`Name: ${PName}`);
-console.log(`Age: ${PAge}`);
